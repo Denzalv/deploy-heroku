aedeb7fe27d68ec25fe694a71c0aaa4cce387c0a
Deni Alpiana <Denzalv01@gmail.com>
2021-12-15 21:24:23 +0700

    update port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import Router from "./routes/routes.js";
 
 const app = express();
 
+const PORT = process.env.PORT || 5000
+
 app.use(express.json());
 
 app.use(cors());
@@ -18,4 +20,4 @@ try {
 
 app.use(Router);
 
-app.listen(5000, () => console.log("Server running at http://localhost:5000"));
+app.listen(PORT, () => console.log(`Server running  at ${PORT}`));
